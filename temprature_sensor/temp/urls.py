91db17984f658f643a7d349970d5f2dd3f46eca0
Trust Nachokwe <130367247+Trust-2-9-na@users.noreply.github.com>
2024-08-22 01:29:34 +0300

    Update urls.py

--- temprature_sensor/temp/urls.py
+++ temprature_sensor/temp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('', index, name='index'),  # The index view that renders sensor data in HTML
   path('api/sensor-data/', SensorDataList.as_view(), name='sensor-data-list'), 
   path('average-sensor-data/', average_sensor_data, name='average_sensor_data'),
+  path('charts/', charts_view, name='charts_view'),
 ]
